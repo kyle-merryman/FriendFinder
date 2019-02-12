4f90f086bad341d9c319d739081fb7da873b80c5
wintermute <kajmerryman@gmail.com>
2019-02-11 17:23:00 -0800

    completed FriendsFinder

---
+++ app/data/friends.js
@@ -0,0 +1,174 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+    {
+      name: "Ross Geller",
+      photo: "http://images.fanpop.com/images/image_uploads/Ross-Geller-ross-geller-322097_624_780.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Monica Geller",
+      photo: "https://vignette.wikia.nocookie.net/p__/images/e/ee/Ms._Gale_Weathers.png/revision/latest?cb=20141118184240&path-prefix=protagonist",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Rachel Green",
+      photo: "http://images.hellogiggles.com/uploads/2015/08/10/Rachel-Green-friends-37542584-864-576.jpg",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Ross Geler",
+      photo: "https://timedotcom.files.wordpress.com/2018/10/david-schwimmer-ross-friends.jpg",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Joey Tribbiani",
+      photo: "https://i.pinimg.com/originals/7c/a8/08/7ca8081d3512d0ce7e50b0a3797ea2b6.jpg",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Phoebe Buffay",
+      photo: "https://sites.google.com/site/friends2365/_/rsrc/1472782816553/home/phoebe-buffay-1/Phoebe-Buffay-psd52229.png",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    },
+    {
+        name: "Gunther",
+        photo: "http://digitalspyuk.cdnds.net/16/39/480x240/landscape-ustv-friends-gunther.jpg",
+        scores: [
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1"
+        ]
+      },
+      {
+        name: "Mr. Heckles",
+        photo: "https://vignette.wikia.nocookie.net/jaygt/images/c/c9/Mrheckles.png/revision/latest?cb=20180206140047",
+        scores: [
+          "5",
+          "5",
+          "5",
+          "5",
+          "5",
+          "5",
+          "5",
+          "5",
+          "5",
+          "5"
+        ]
+      },
+      {
+        name: "Ross",
+        photo: "https://gl-images.condecdn.net/image/oQYy7b8zjXN/crop/405/f/ross-geller_glamour_10aug17_cbs-sky_p.jpg",
+        scores: [
+          "1",
+          "5",
+          "1",
+          "5",
+          "1",
+          "5",
+          "1",
+          "5",
+          "1",
+          "5"
+        ]
+      },
+      {
+        name: "Ted Moesby",
+        photo: "http://i.imgur.com/R5xIL.png",
+        scores: [
+          "3",
+          "3",
+          "3",
+          "3",
+          "3",
+          "3",
+          "3",
+          "3",
+          "3",
+          "3"
+        ]
+      }
+      
+  
+  ];
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
+  
